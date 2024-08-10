1dc13764dafd42294f83dc6870f35f23cc34c85f
Kundan Lamse <kundanlamse19@gmail.com>
2024-08-10 15:53:21 +0530

    Initial class design without add_book method being defined

---
+++ Library.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+class LibraryManagementSystem:
+    def __init__(self):
+        self.host='localhost'
+        self.username='root'
+        self.password='root'
+        self.database=""
+
+        self.conn=mysql.connector.connect(
+            host=self.host,
+            user=self.username,
+            password=self.password,
+            database=self.database
+
+        )
+    
+    def add_book(self,ISBN,Author,Title,Year,Count):
+        pass
