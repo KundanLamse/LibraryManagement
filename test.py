fd8606be413541c2f2fbd31b6526e2260bdb9e81
Kundan Lamse <kundanlamse19@gmail.com>
2024-08-10 23:59:05 +0530

    Initial failed test for view_books method

--- test.py
+++ test.py
@@ -21,7 +21,7 @@ def test_add_book():
     )
     mock_conn.commit.assert_called_once()
 
-def test_borrow_success():
+def test_borrow_success(): #Test case that successful borrow of available book
     LMS,mock_cursor,mock_conn=conn()
     mock_cursor.fetchone.return_value = (5,)
     result = LMS.book_borrow('123-456-789')
@@ -33,3 +33,33 @@ def test_borrow_success():
     )
     mock_conn.commit.assert_called_once()
     assert result is True
+def test_borrow_failure():
+    LMS,mock_cursor,mock_conn=conn()
+    mock_cursor.fetchone.return_value = (0,)
+    result=LMS.book_borrow('123-456-789')
+    with pytest.raises(ValueError, match="Book is not available"):
+        LMS.book_borrow('123-456-789')
+    
+    # Verify that the correct SQL commands were executed
+    mock_cursor.execute.assert_any_call(
+        "SELECT Count FROM books WHERE ISBN = %s", ('123-456-789',)
+    )
+    
+    # Ensure no update was attempted or committed
+    mock_cursor.execute.assert_not_called_with(
+        "UPDATE books SET Count = Count - 1 WHERE ISBN = %s", ('123-456-789',)
+    )
+    mock_conn.commit.assert_not_called()
+    assert result is False
+def test_view_books():
+    LMS,mock_cursor,_=conn()
+    mock_cursor.fetchall.return_value = [
+        ('123-456-789', 'William', 'Othello', 1622, 5)]
+    expected_books = [
+        {'ISBN': '123-456-789', 'Author': 'William', 'Title': 'Othello', 'Year': 1622, 'Count': 5}
+           ]
+    result = LMS.view_books()
+    assert result == expected_books
+    # Verify the database interaction
+    mock_cursor.execute.assert_called_once_with("SELECT * FROM books WHERE Count > 0")
+    
